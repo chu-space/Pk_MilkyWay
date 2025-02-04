a07ee5020cbb82731afc75e16e32f602702df69f
achu123 <achu123@uw.edu>
2025-02-04 19:34:39 +0800

    Figure 5 looks good

--- plot_SHMF.py
+++ plot_SHMF.py
@@ -5,11 +5,12 @@ import pandas as pd
 from pandas import DataFrame
 import beyond_CDM_SHMF
 
+# Base paths for COZMIC 1 host haloes
 BASE_PATH_HALO4 = '/central/groups/carnegie_poc/enadler/ncdm_resims/Halo004/'
 BASE_PATH_HALO113 = '/central/groups/carnegie_poc/enadler/ncdm_resims/Halo113/'
 BASE_PATH_HALO023 = '/central/groups/carnegie_poc/enadler/ncdm_resims/Halo023/'
 
-
+# Accessing the three host halos from the COZMIC 1 suite of simulations
 halos_wdm3_004 = readHlist(BASE_PATH_HALO4 + 'wdm_3/output_wdm_3/rockstar/hlists/hlist_1.00000.list')
 host_wdm3_004 = halos_wdm3_004[halos_wdm3_004['id'] == 1889038]
 subhalos_wdm3_004 = halos_wdm3_004[halos_wdm3_004['upid']==host_wdm3_004['id']]
@@ -42,81 +43,85 @@ halos_wdm10 = readHlist(BASE_PATH_HALO4 + 'wdm_10/output_wdm_10/rockstar/hlists/
 host_wdm10 = halos_wdm10[halos_wdm10['id'] == 5142328]
 subhalos_wdm10 = halos_wdm10[halos_wdm10['upid']==host_wdm10['id']]
 
+# Constants
 analysis = 'Mpeak'
 cut = 'Mvir'
 h_cdm = 0.7
 threshold = 1.2*10**8*h_cdm
 
+# Filtering all subhaloes based on what we are analysing: Mpeak - the peak mass
 mass_peak_sub_wdm3_004 = subhalos_wdm3_004[:][analysis]
 mass_peak_sub_wdm3_113 = subhalos_wdm3_113[:][analysis]
 mass_peak_sub_wdm3_023 = subhalos_wdm3_023[:][analysis]
 
-#cond_wdm3_004 = mass_sub_wdm3_004 > threshold
-#cond_wdm3_113 = mass_sub_wdm3_113 > threshold_1
-#cond_wdm3_023 = mass_sub_wdm3_023 > threshold_1
-
-#mean_count_wdm3 = (count_wdm3_004+count_wdm3_113+count_wdm3_023)/3
-#print("Number of averaged subhaloes masses of 3keV WDM that surpassed the " + str(analysis) + " threshold " + str(threshold) + " is: " + str(mean_count_wdm3))
-
-# Filter subhaloes
+# Making a cut based on: Mvir - virial mass
 mass_subhalos_wdm3_Mvir_004 = subhalos_wdm3_004[:][cut]
 mass_subhalos_wdm3_Mvir_113 = subhalos_wdm3_113[:][cut]
 mass_subhalos_wdm3_Mvir_023 = subhalos_wdm3_023[:][cut]
 
+# Getting a binary condition if the mass of subhaloes surpass a threshold
 cond_wdm3_Mvir_cut_004 = mass_subhalos_wdm3_Mvir_004 > threshold
-mass_peak_with_Mvir_cut_004 = mass_peak_sub_wdm3_004[cond_wdm3_Mvir_cut_004]
 cond_wdm3_Mvir_cut_113 = mass_subhalos_wdm3_Mvir_113 > threshold
-mass_peak_with_Mvir_cut_113 = mass_peak_sub_wdm3_113[cond_wdm3_Mvir_cut_113]
 cond_wdm3_Mvir_cut_023 = mass_subhalos_wdm3_Mvir_023 > threshold
+
+# Applying the condition so that we only get the peak masses of subhaloes with a certain virial mass
+mass_peak_with_Mvir_cut_004 = mass_peak_sub_wdm3_004[cond_wdm3_Mvir_cut_004]
+mass_peak_with_Mvir_cut_113 = mass_peak_sub_wdm3_113[cond_wdm3_Mvir_cut_113]
 mass_peak_with_Mvir_cut_023 = mass_peak_sub_wdm3_023[cond_wdm3_Mvir_cut_023]
 
-# Differential SHMF
+# Differential SHMF: Getting the merged mean of all the peak masses made with this cut
 df1 = DataFrame(mass_peak_with_Mvir_cut_004)
 df2 = DataFrame(mass_peak_with_Mvir_cut_113)
 df3 = DataFrame(mass_peak_with_Mvir_cut_023)
+
+# Merge the data frames and take the mean of them
+# This is where statistical errors may take place
 df_merged = pd.concat([df1, df2, df3])
 by_row_index = df_merged.groupby(df_merged.index)
 df_merged_mean = by_row_index.mean()
 
+# Merged array of virial mass
+# This is where statistical errors may take place (overcounting?)
 df_merged_Mvir = pd.concat([DataFrame(mass_subhalos_wdm3_Mvir_004), DataFrame(mass_subhalos_wdm3_Mvir_113), DataFrame(mass_subhalos_wdm3_Mvir_023)])
-#print(df_merged_Mvir[0][0][0])
-# Define mass bins (log scale between 10**7 to 11)
-#log_bins = np.linspace(7, 12, 12)
+
+# Define mass bins
 log_bins = np.linspace(7, 12, 10)
 bins = 10**log_bins
-#upper limit is 1/2 mvir host
-diff_log_bins = np.logspace(np.log10(1.2e8),np.log10(1.2e11),10)
-#diff_bins = 10**diff_log_bins
+
+# Ethan's mass log bin choice - upper limit is 0.5*M_vir,host
+diff_log_bins = np.logspace(np.log10(1.2e8),np.log10(mass_subhalos_wdm3_Mvir_004[0]/(2.*0.7)),10)
+# Differential bin centers for plotting
 diff_bin_centers = 0.5 * (diff_log_bins[1:] + diff_log_bins[:-1])
 
-x3 = np.log10(diff_log_bins)
-
-print('diff_log_bins: ', diff_log_bins)
-print('df_merged_mean.values: ', df_merged_mean.values)
-diff_hist, _ = np.histogram(df_merged_mean.values, bins=bins)
+diff_hist, _ = np.histogram(df_merged_mean.values, bins=diff_log_bins)
 differential_shmf = diff_hist / (np.diff(log_bins))  # Normalize by bin width (log space)
 
+# Cummulative log bin choice
 cumm_log_bins = np.linspace(7.5,11,10)
 cumm_bins = 10**cumm_log_bins
-cumm_bin_centers = 0.5 * (cumm_bins[1:] + cumm_bins[:-1])
 
-cumm_hist, _ = np.histogram(df_merged_mean.values, bins=cumm_bins)
+# Cummulative bin centers for plotting
+cumm_bin_centers = 0.5 * (cumm_bins[1:] + cumm_bins[:-1])
+cumm_hist, _ = np.histogram(df_merged_mean.values/0.7, bins=cumm_bins)
 
 # Cumulative SHMF
 cumulative_shmf = np.cumsum(cumm_hist[::-1])[::-1]  # Reverse cumulative sum
-print("cumulative_shmf: ", cumulative_shmf)
+
 # Differential WDM SHMF
 beyond_CDM_SHMF.f_beyond_CDM(df_merged.values, 10, 2.5, 0.9, 1.0)
 
+# COZMIC 1 paper data
 x_cumm = [102562564.77081062, 189508862.66108668, 466442535.165321, 887775605.6633633, 1679198194.49203, 2780024594.2117653, 6724184096.911058, 16411385971.133947, 25370856514.077957]
-y_cumm = [36.37097470752195, 36.63411357730507, 29.065641353815458, 21.70185477434134, 15.994894342266116, 12.61964315649359, 6.319753166844106, 3.011238449807771, 1.015283760417585]
+y_cumm = [37.        , 36.33333333, 29.33333333, 19.33333333, 12.66666667,
+         6.33333333,  3.        ,  0.33333333,  0.33333333]
 
-y_diff = [37.56934306569343, 36.819951338199516, 25.83698296836983, 18.54501216545012, 9.722627737226276]
+y_diff = [17.        , 28.66666667, 18.        ,  5.66666667,  6.66666667,
+         2.66666667,  0.66666667,  0.33333333]
 x_diff = [10**8,
-2362256814.4919534,
-12646716360.5118,
-16218909592.18256,
-19791102823.853317]
+0.5*10**9,
+10**9,
+0.5*10**10,
+10**10, 0.5*10**11, 10**11, 0.5*10**12]
 
 # Plot results
 plt.figure(figsize=(10, 6))
@@ -124,7 +129,7 @@ plt.figure(figsize=(10, 6))
 # Differential plot
 plt.subplot(1, 2, 1)
 plt.plot(diff_bin_centers, differential_shmf, label="Differential SHMF")
-plt.plot(x_cumm,y_cumm, label="Cozmic Differential")
+plt.plot(x_diff,y_diff, label="Cozmic Differential")
 plt.xscale('log')
 plt.yscale('log')
 plt.xlabel(r'$M_{\mathrm{sub}}$')
@@ -134,7 +139,7 @@ plt.legend()
 # Cumulative plot
 plt.subplot(1, 2, 2)
 plt.plot(cumm_bin_centers, cumulative_shmf, label="Cumulative SHMF")
-plt.plot(x_diff,y_diff, label="Cozmic Cumulative")
+plt.plot(x_cumm,y_cumm, label="Cozmic Cumulative")
 plt.xscale('log')
 plt.xlabel(r'$M_{\mathrm{sub}}$')
 plt.ylabel(r'$N(>M)$')
@@ -143,8 +148,3 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 plt.savefig('Diff_Cumm_SHMF_plot.png')
-
-#f = plt.figure()
-#plt.plot(range(10**8, 10**11,10), count_wdm3)
-#plt.show()
-#f.savefig("SHMF_WDM.pdf", bbox_inches='tight')
