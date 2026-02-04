c5e8f3c5f37dee58cf4acbb417d2d7346b02f00c
Vera Gluscevic <vera.gluscevic@gmail.com>
2026-02-04 16:28:33 -0500

    vg cdm tests success

--- vg_tests_oct2025/vg_plot.py
+++ vg_tests_oct2025/vg_plot.py
@@ -1,24 +1,48 @@
 import numpy as np
+from scipy.interpolate import Akima1DInterpolator
 import matplotlib.pyplot as plt
 
 index = 1
-offset = 0
+offset = 1
+
+#cdm = True; dmeff = False
+dmeff = True; cdm = False
 
 plt.figure()
 #plt.title('n=2, 1e-2GeV, envelope')
 
-#data_camb = np.loadtxt('data_tk/idm_n2_1e-2GeV_envelope_z99_Tk.dat')
-data_camb = np.loadtxt('data_tk/test_transfer_z99.dat')
-data_class = np.loadtxt('output/explanatory01_tk.dat')
+if dmeff:
+    data_camb = np.loadtxt('data_tk/idm_n2_1e-2GeV_envelope_z99_Tk.dat')
+    data_class = np.loadtxt('output/explanatory01_tk.dat')
+if cdm:
+    data_camb = np.loadtxt('data_tk/test_transfer_z99.dat')
+    data_class = np.loadtxt('output/explanatoryCDM_tk.dat')
+    #data_class = np.loadtxt('output/calibration_run-vg07_tk.dat')
 
 
 kc = data_camb[:,0]
 tkc = data_camb[:, index]
 
 k = data_class[:,0]
-tk = data_class[:,index+offset]
+tk = data_class[:, index+offset]
 
-tkc_interp = np.interp(k, kc, tkc)
+
+# Hybrid interpolation: use log-log for positive data, Akima for oscillatory/negative
+# Clip k to valid range to prevent extrapolation artifacts at boundaries
+k_min, k_max = kc.min(), kc.max()
+k_clipped = np.clip(k, k_min, k_max)
+
+# Check if all values are positive (use log-log interpolation if so)
+if np.all(tkc > 0) and np.all(kc > 0):
+    # Log-log interpolation: best for smooth positive transfer functions
+    tkc_interp = np.exp(np.interp(np.log(k_clipped), np.log(kc), np.log(tkc)))
+else:
+    # Akima interpolation: handles negative values and oscillatory behavior
+    sort_idx = np.argsort(kc)
+    kc_sorted = kc[sort_idx]
+    tkc_sorted = tkc[sort_idx]
+    akima_interp = Akima1DInterpolator(kc_sorted, tkc_sorted)
+    tkc_interp = akima_interp(k_clipped)
 
 plt.loglog(kc, tkc**2, color='blue',label='camb')
 plt.loglog(k, tk**2, color='red', label='class')
