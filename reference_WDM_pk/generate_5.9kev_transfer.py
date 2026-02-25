e67d84cbb23ca0c5c7cedec1ee17bfda0a4f3027
Arif Chu <arifchu@hpc-22-18.cm.cluster>
2026-02-25 10:22:27 -0800

    cleaning up old tests and including a accessibly npy version of a new 5.9kev WDM constraint

---
+++ reference_WDM_pk/generate_5.9kev_transfer.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+# Load the base files 
+wdm_65 = np.load('6.5_wdm_transfer.npy', allow_pickle=True)
+cdm_pk = np.load('cdm_transfer.npy', allow_pickle=True)
+
+k = wdm_65[:, 0]
+cdm_vals = cdm_pk
+
+# The parameterisation for 5.9 keV WDMi (our new WDM bounds) according to COZMIC 1
+m_wdm = 5.9
+omega_mh2 = 0.11711
+h = 0.7
+A = 0.0437
+B = -1.188
+nu = 1.049
+theta = 2.012
+eta = 0.2463
+
+# Calculate the WDM transfer function ratio
+alpha = A * (m_wdm**B) * ((omega_mh2 / 0.12)**eta) * ((h / 0.6736)**theta)
+Pk_ratio = (1 + (alpha * k)**(2 * nu))**(-5.0 / nu)
+
+# Multiply the CDM transfer function by the cutoff ratio to get WDM
+wdm_59_vals = cdm_vals * Pk_ratio
+
+# Stack the k-values and new WDM transfer function together and ensure format matches original
+wdm_59 = np.column_stack((k, wdm_59_vals)).astype(np.float64)
+
+# Save to the new NPY file safely
+np.save('5.9_wdm_transfer.npy', wdm_59)
+
